5ba5908b571b8a1821957ce23836d373203e0a09
Simmba <simmern.kang@gmail.com>
2017-06-03 13:43:35 -0700

    sound implementation

--- index.vr.js
+++ index.vr.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, asset, Pano, View, Text, StyleSheet, Box, VideoPano, Sound } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text, StyleSheet, Box, VideoPano, Sound, VrButton } from 'react-vr';
 import axios from 'axios';
 
 import BouncingText from './components/bouncingText';
@@ -173,6 +173,13 @@ class Basics extends Component {
         <View>
           <Text style={styles.currentText}>{this.state.keyboardText}</Text>
           <Text style={styles.historyText}>{this.state.messageText}</Text>
+          <View>
+          {this.state.context === "completed" ?
+          <Sound source={asset('woot.wav')} />
+          :
+          <VrButton />
+          }
+          </View>
         </View>
         <TopplingList toppings={this.state.toppings} submitOrder={this.placeOrder.bind(this)}/>
         {this.state.showTracker === true ? <Timer showTracker={this.state.showTracker}/> : null}
